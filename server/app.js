219d388bbb34299f38dda8e1b7e666d7ba976128
jonmds <meidus8@gmail.com>
2016-08-18 12:15:05 +0300

    Server separation

---
+++ server/app.js
@@ -0,0 +1,28 @@
+module.exports = function() {
+    var express = require('express');
+    var helmet = require('helmet');
+    var app = express();
+    app.use(helmet());
+    app.disable('x-powered-by');
+
+    var bodyParser = require('body-parser');
+
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/contacts');
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+// Use only for development mode
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        next();
+    });
+
+    var contacts = require('./routes/contact.js')(app);
+    return app;
+};
